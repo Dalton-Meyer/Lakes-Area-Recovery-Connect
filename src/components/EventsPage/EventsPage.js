e98f607ec4bedfd11fdad6f721b63f4392d5f065
Dalton Meyer <dalton.meyer94@gmail.com>
2020-07-10 11:35:19 -0500

    added access level to events

--- src/components/EventsPage/EventsPage.js
+++ src/components/EventsPage/EventsPage.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Table, Pane } from 'evergreen-ui'
+import { Table, Pane, TextInput, Select } from 'evergreen-ui'
 import './EventsPage.css'
 
 
 // this is the header component that displays on every page
 
 class Events extends Component {
+    state = {
+        name: '',
+        location: '',
+        type: 1,
+        user: '',
+    }
+    componentDidMount() {
+        this.setState({ user: this.props.user.user_access })
+
+    }
     render() {
         return (
             <Pane
@@ -15,34 +25,80 @@ class Events extends Component {
                 alignItems="center"
                 justifyContent="center"
                 border="default">
+                {Number(this.props.user.user_access) > 1 ?
                     <Pane
-                    width='70%'
-                    height={800}
-                    display="flex"
-                    justifyContent="center"
-                    border="default">
-            <div className='events'>
-                <h1>All Events</h1>
-                <Table>
-                    <Table.Head>
-                        <Table.TextHeaderCell>
-                            Event
-          </Table.TextHeaderCell>
-                        <Table.TextHeaderCell>
-                            Location
-          </Table.TextHeaderCell>
-                        <Table.TextHeaderCell>
-                            Date
-          </Table.TextHeaderCell>
-                    </Table.Head>
-                    <Table.Body>
-                        <Table.Row>
+                        width='70%'
+                        height={800}
+                        display="flex"
+                        justifyContent="center"
+                        border="default">
+                        <div className='events'>
+                            <h1>All Events</h1>
+                            <h2>Add Event:</h2>
+                            <TextInput
+                                onChange={e => this.setState({ name: e.target.value })}
+                                value={this.state.name}
+                                placeholder="Event Name"
+                            />
+                            <TextInput
+                                onChange={e => this.setState({ location: e.target.value })}
+                                value={this.state.location}
+                                placeholder="Event Location"
+                            />
+                            <Select value={this.state.type} onChange={event => this.setState({ type: event.target.value })}>
+                                <option value="1" selected>AA</option>
+                                <option value="2">NA</option>
+                                <option value="3">Other</option>
+                            </Select>
+                            <Table>
+                                <Table.Head>
+                                    <Table.TextHeaderCell>
+                                        Event
+                                    </Table.TextHeaderCell>
+                                    <Table.TextHeaderCell>
+                                        Location
+                                    </Table.TextHeaderCell>
+                                    <Table.TextHeaderCell>
+                                        Date
+                                    </Table.TextHeaderCell>
+                                </Table.Head>
+                                <Table.Body>
+                                    <Table.Row>
 
-                        </Table.Row>
-                    </Table.Body>
-                </Table>
-            </div>
-            </Pane>
+                                    </Table.Row>
+                                </Table.Body>
+                            </Table>
+                        </div>
+                    </Pane>
+
+                    : <Pane
+                        width='70%'
+                        height={800}
+                        display="flex"
+                        justifyContent="center"
+                        border="default">
+                        <div className='events'>
+                            <h1>All Events</h1>
+                            <Table>
+                                <Table.Head>
+                                    <Table.TextHeaderCell>
+                                        Event
+                                    </Table.TextHeaderCell>
+                                    <Table.TextHeaderCell>
+                                        Location
+                                    </Table.TextHeaderCell>
+                                    <Table.TextHeaderCell>
+                                        Date
+                                </Table.TextHeaderCell>
+                                </Table.Head>
+                                <Table.Body>
+                                    <Table.Row>
+
+                                    </Table.Row>
+                                </Table.Body>
+                            </Table>
+                        </div>
+                    </Pane>}
             </Pane>
         )
     }
@@ -50,7 +106,7 @@ class Events extends Component {
 
 const mapStateToProps = (state) => {
     return {
-
+        user: state.user
     }
 }
 export default connect(mapStateToProps)(Events);
